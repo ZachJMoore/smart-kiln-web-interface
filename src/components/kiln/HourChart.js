8aa04cae68c13834401c4c12885a144cfbe2df61
ZachMoore <Get3Moore@gmail.com>
2018-05-19 14:46:46 -0400

    Add hour chart
    Use chart.js to include a chart for displaying the past hours temp log

--- src/components/kiln/HourChart.js
+++ src/components/kiln/HourChart.js
@@ -1,12 +1,63 @@
 import React, { Component } from "react"
+import Chart from "chart.js"
 
 class HourChart extends Component{
+    constructor(props){
+        super(props);
+        this.initChart = (data, label)=>{
+            var chart = new Chart(this.refs.ctx, {
+                type: 'line',
+                data: {
+                    labels: label,
+                    datasets: [{
+                        data: data,
+                        fill: true,
+                        borderColor: "#f0eeea",
+                        backgroundColor: "#fff5"
+                    }]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: "Hour Chart",
+                        fontSize: 16,
+                        fontColor: "#f0eeea"
+                    },
+                    responsive: true,
+                    tooltips: {
+                        mode: "index",
+                        intersect: false
+                    },
+                    hover: {
+                        mode: "nearest",
+                        intersect: true
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: false
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+    }
+
+    componentDidMount(){
+        let data = this.props.data.slice().reverse()
+        let label = data.map((temp, index)=>{
+            return `${((index*5)+5)}m`
+        }).reverse()
+        this.initChart(data, label)
+    }
     render(){
-        return(<ul className="flex-container-center flex-stretch flex-wrap" style={{listStyle: "none", padding: "0"}}>
-            {this.props.data.map((data, index)=>{
-                return <li key={index} className="light-shade" style={{margin: "5px"}}>{data}</li>
-            })}
-        </ul>)
+        return(<div className="flex-container-center column hour-chart-container flex-stretch">
+            <canvas ref="ctx" className="hour-chart-canvas"></canvas>
+        </div>)
     }
 }
 
--- src/components/kiln/KilnConsole.js
+++ src/components/kiln/KilnConsole.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import HourChart from "./HourChart";
+import { Route, Link, withRouter} from "react-router-dom"
 
 class KilnConsole extends Component{
     render(){
@@ -8,10 +9,25 @@ class KilnConsole extends Component{
             <span className="light-shade">{this.props.kiln.metadata.name}</span>
         </section>
         <section className="view-body flex-container ai-center column" >
-            {this.props.kiln.tempLog && <HourChart data={this.props.kiln.tempLog}/>}
+            <section className="siimple-tabs flex-container-center space-around">
+                <Link to={`${this.props.match.url}/recent`} className="siimple-tabs-tab light-shade decoration-none">Recent</Link>
+                <Link to={`${this.props.match.url}`} className="siimple-tabs-tab light-shade decoration-none">Overview</Link>
+                <Link to={`${this.props.match.url}/controls`} className="siimple-tabs-tab light-shade decoration-none">Controls</Link>
+            </section>
+            <Route exact path={`${this.props.match.url}/recent`} render={()=>{
+                return <span className="center-snippet">Recent</span>
+            }} />
+            <Route exact path={`${this.props.match.url}`} render={()=>{
+                return (<div className="flex-stretch">
+                    {this.props.kiln.tempLog && <HourChart data={this.props.kiln.tempLog}/>}
+                </div>)
+            }} />
+            <Route exact path={`${this.props.match.url}/controls`} render={()=>{
+                return <span className="center-snippet">Controls</span>
+            }} />
         </section>
     </div>)
     }
 }
 
-export default KilnConsole
+export default withRouter(KilnConsole)
